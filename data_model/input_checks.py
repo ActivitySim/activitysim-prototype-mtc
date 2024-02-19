69bcda5c5c8d8e8384876fe55299455acf50aef6
David Hensle <david.hensle@rsginc.com>
2024-02-19 09:39:04 -0800

    fixing test example
    added example_hwy_data for input checker, inefficient input_checker test updated, necessary settings.

--- data_model/input_checks.py
+++ data_model/input_checks.py
@@ -225,12 +225,13 @@ class Landuse(pa.DataFrameModel):
     def check_pop_per_zone(cls, land_use: pd.DataFrame):
         persons = TABLE_STORE["persons"]
         households = TABLE_STORE["households"]
-        pop = persons.groupby(
-            persons.household_id.map(
-                lambda hhid: households.set_index("household_id").home_zone_id[hhid]
-            )
-        ).person_id.nunique()
-        return (pop == land_use.set_index("zone_id").TOTPOP).reindex(land_use.index)
+        persons_per_household = persons.groupby("household_id").size()
+        hh = households[["household_id", "home_zone_id"]].merge(
+            persons_per_household.rename("persons_per_household"), on="household_id"
+        )
+        pop = hh.groupby(households.home_zone_id)["persons_per_household"].sum()
+        lu = land_use.set_index("zone_id")
+        return pop.reindex(lu.index) == lu.TOTPOP
 
 
 class NetworkLinks(pa.DataFrameModel):
